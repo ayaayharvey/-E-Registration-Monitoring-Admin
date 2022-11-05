abe1bbf0d16b92dcb23d0bdc55e25d78ae32181c
ayaayharvey <ayaayharvey@gmail.com>
2022-11-06 07:08:19 +0800

    Enhanced Sidebar Navigations

--- src/components/sidebar/navigations/Navigation.vue
+++ src/components/sidebar/navigations/Navigation.vue
@@ -1,37 +1,55 @@
 <template>
-  <div
-    class="flex justify-between px-3 py-2 hover:bg-white hover:text-gray-700 hover:font-semibold rounded content-center transition"
-  >
-    <div class="flex">
-      <span class="pr-2">
-        <i :class="navigation.icon"></i>
-      </span>
-      <h3 class="font-semibold">
-        {{ navigation.title }}
-      </h3>
-    </div>
-    <span class="pr-2 right" v-show="navigation.child.length !== 0">
-      <i class="fas fa-angle-down"></i>
-    </span>
-  </div>
-  <div v-for="child in navigation.child" class="px-3">
-    <router-link
-      :to="child.path"
-      class="flex px-3 py-1 hover:bg-white hover:text-gray-700 hover:font-semibold rounded cursor-pointer transition"
-    >
-      <span class="pr-3">•</span>
-      <h3 class="font-semibold">
-        {{ child.title }}
-      </h3>
-    </router-link>
-  </div>
+	<button
+		@click="toggleChild()"
+		class="w-full hover:bg-white hover:text-gray-700 hover:font-semibold flex justify-between px-3 py-2 rounded content-center transition"
+	>
+		<div class="flex">
+			<span class="pr-2">
+				<i :class="navigation.icon"></i>
+			</span>
+			<h3 class="font-semibold">
+				{{ navigation.title }}
+			</h3>
+		</div>
+		<span
+			class="pr-2 right"
+			v-show="navigation.child.length !== 0"
+		>
+			<i :class="`${showChild ? 'fas fa-angle-down' : 'fas fa-angle-up'}`"></i>
+		</span>
+	</button>
+	<button
+		v-show="showChild"
+		v-for="child in navigation.child"
+		class="w-full px-3"
+	>
+		<router-link
+			:to="child.path"
+			class="flex px-3 py-1 hover:bg-white hover:text-gray-700 hover:font-semibold rounded cursor-pointer transition"
+		>
+			<span class="pr-3">•</span>
+			<h3 class="font-semibold">
+				{{ child.title }}
+			</h3>
+		</router-link>
+	</button>
 </template>
 
 <script>
 export default {
-  name: 'Navigation',
-  props: {
-    navigation: Object,
-  },
+	name: "Navigation",
+	props: {
+		navigation: Object,
+	},
+	data() {
+		return {
+			showChild: false,
+		};
+	},
+	methods: {
+		toggleChild() {
+			this.showChild = !this.showChild;
+		},
+	},
 };
 </script>
--- src/components/sidebar/navigations/Navigations.vue
+++ src/components/sidebar/navigations/Navigations.vue
@@ -1,67 +1,89 @@
 <template>
-  <div class="py-5">
-    <div v-for="navigation in navigations">
-      <div v-if="navigation.child.length === 0">
-        <router-link :to="navigation.path" class="cursor-pointer">
-          <Navigation :navigation="navigation" />
-        </router-link>
-      </div>
-      <div v-else>
-        <Navigation :navigation="navigation" />
-      </div>
-    </div>
-  </div>
+	<div class="py-5">
+		<div v-for="navigation in navigations">
+			<div v-if="navigation.child.length === 0">
+				<router-link
+					:to="navigation.path"
+					class="cursor-pointer"
+				>
+					<Navigation :navigation="navigation" />
+				</router-link>
+			</div>
+			<div v-else>
+				<Navigation :navigation="navigation" />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import Navigation from "./Navigation.vue";
 export default {
-  name: "Navigations",
-  data() {
-    return {
-      navigations: [],
-    };
-  },
-  components: {
-    Navigation,
-  },
-  created() {
-    this.navigations = [
-      {
-        title: "Dashboard",
-        icon: "fas fa-chart-area",
-        path: "/",
-        child: [],
-      },
-      {
-        title: "Transactions",
-        icon: "fas fa-user",
-        path: "/transactions",
-        child: [],
-      },
-      {
-        title: "Tickets",
-        icon: "fas fa-user",
-        path: "/tickets",
-        child: [],
-      },
-      // {
-      //   title: "Expense Management",
-      //   icon: "fas fa-calculator  ",
-      //   child: [
-      //     {
-      //       title: "Expense Categories",
-      //       icon: "fas fa-circle",
-      //       path: "/expense-management/expense-categories",
-      //     },
-      //     {
-      //       title: "Expenses",
-      //       icon: "fas fa-circle",
-      //       path: "/expense-management/expenses",
-      //     },
-      //   ],
-      // },
-    ];
-  },
+	name: "Navigations",
+	data() {
+		return {
+			navigations: [],
+		};
+	},
+	components: {
+		Navigation,
+	},
+	created() {
+		this.navigations = [
+			{
+				title: "Dashboard",
+				icon: "fas fa-chart-area",
+				path: "/",
+				child: [],
+			},
+			{
+				title: "Master",
+				icon: "fas fa-calculator  ",
+				child: [
+					{
+						title: "Page Master",
+						icon: "fas fa-circle",
+						path: "/master/page",
+					},
+					{
+						title: "Event Category",
+						icon: "fas fa-circle",
+						path: "/master/event-category",
+					},
+					{
+						title: "Payment Category",
+						icon: "fas fa-circle",
+						path: "/master/payment-category",
+					},
+					{
+						title: "Ticket Category",
+						icon: "fas fa-circle",
+						path: "/master/ticket-category",
+					},
+					{
+						title: "Access",
+						icon: "fas fa-circle",
+						path: "/master/access",
+					},
+					{
+						title: "Event Status",
+						icon: "fas fa-circle",
+						path: "/master/event-status",
+					},
+					{
+						title: "Event Rate Percentage",
+						icon: "fas fa-circle",
+						path: "/master/event-rate-percentage",
+					},
+				],
+			},
+			{
+				title: "Tickets",
+				icon: "fas fa-user",
+				path: "/tickets",
+				child: [],
+			},
+		];
+	},
 };
 </script>
