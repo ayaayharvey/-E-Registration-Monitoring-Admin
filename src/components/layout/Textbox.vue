ab9c74761ac6dcb1eb525d465c3cdb4610100b0f
ayaayharvey <ayaayharvey@gmail.com>
2022-11-07 08:43:17 +0800

    Updated Textbox Functionality

--- src/components/layout/Textbox.vue
+++ src/components/layout/Textbox.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="py-2 w-full">
-    <label class="text-sm">{{ label }}</label>
-    <div class="mt-1">
-      <input
-        type="textbox"
-        class="py-1 border-2 border-gray-500 rounded w-full px-3 placeholder:text-xs"
-        :placeholder="placeholder"
-      />
-    </div>
-  </div>
+	<div class="w-full">
+		<label class="text-gray-600 text-sm font-semibold">{{ label }}</label>
+		<div class="mt-1">
+			<input
+				type="textbox"
+				:value="value"
+				class="mt-1 w-full border rounded bg-gray-50 border-gray-400 px-2 py-1 placeholder:text-xs"
+				:class="`${disabled && 'bg-gray-200 cursor-not-allowed'}`"
+				:placeholder="`${disabled ? '' : placeholder}`"
+				:disabled="disabled"
+			/>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'Textbox',
-  props: {
-    label: String,
-    placeholder: String,
-  },
+	name: "Textbox",
+	props: {
+		label: String,
+		value: String,
+		placeholder: String,
+		disabled: Boolean,
+	},
 };
 </script>
