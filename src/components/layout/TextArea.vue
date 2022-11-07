b9464051ad54843377747d4ca1709687c4d4be21
ayaayharvey <ayaayharvey@gmail.com>
2022-11-07 08:42:59 +0800

    Added TextArea Component

---
+++ src/components/layout/TextArea.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="w-full">
+		<label class="text-gray-600 text-sm font-semibold">{{ label }}</label>
+		<div class="mt-1">
+			<textarea
+				type="textbox"
+				:value="value"
+				class="mt-1 w-full border rounded bg-gray-50 border-gray-400 px-2 py-1 placeholder:text-xs"
+				:class="`${disabled && 'bg-gray-200 cursor-not-allowed'}`"
+				:placeholder="`${disabled ? '' : placeholder}`"
+				:disabled="disabled"
+			/>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "TextArea",
+	props: {
+		label: String,
+		value: String,
+		placeholder: String,
+		disabled: Boolean,
+	},
+};
+</script>
