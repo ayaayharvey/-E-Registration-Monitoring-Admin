286214f789751248046ffb9d05a30510002c5585
ayaayharvey <ayaayharvey@gmail.com>
2022-11-09 05:09:00 +0800

    updated pagination functionality

--- src/stores/EventCategoryStore.js
+++ src/stores/EventCategoryStore.js
@@ -3,7 +3,8 @@ import { defineStore } from "pinia";
 export const useEventCategoryStore = defineStore("eventCategoryStore", {
 	state: () => ({
 		token:
-			"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5N2IwOGI1OC00MzJlLTQxMjItYjBhNi02N2JiMmZkOTdjY2IiLCJqdGkiOiI4ZWViYTFmZGQzNzNiYmRhMGUzYmFjOTQ0NDE4YjJkZjM3NmZmZmQ4N2ZlNjRmMmExNzkxMjdjYWU1ZDM4YThiNWE1MjdhOWE5YjdlNzNkYiIsImlhdCI6MTY2Nzg4NDA3OC4zMDE0MTIsIm5iZiI6MTY2Nzg4NDA3OC4zMDE0MTYsImV4cCI6MTY2NzkyMDA3OC4yOTMyNDgsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.Vk_ZvSPVsO290LHYZUIcnROh2BAhqHXvwtyuzCYBKcCIgYLlsLqqt9PHh-jydrt9DH0GdNhCkcju9PaaM14_9aXW7rC3gSZCt8S74bncPJgxSKgalRcQz1dZB_UMYu31BthgZqjwCzRVyawP0d6ETCAOL36Iu_iCckDrjeOwm7pRPhBOutHmKgB1RfgN_GxieExxQW1oMXvGUSQWdBuEqbTVJh47pVT40hXkFR3xZ5sgwB2Uuqc8ZqOFDzwFcJsHbQRsUNQvzxst5zqTYYEF4Bb4tm9IxAjqwuVNsok4zc-kVFU2-Lpki3gZS8RaPgggmlaFh42wGUYO0BWghTl1lNjF02FE_cHRD1VFB7yjwCB-JFR52qIyXOzhfP-0aDhQrPc7_WSSv0IhXYPMwHtR1EDsz5pPT_XmQ4_18vr_7nBbIB4sYp2lbAKqtxvvNwnlc9PWHIv5eYqdvrzxC1nsPnafZ1sGuv6mFtwe7Y3A94-2j0JUkZ4KIsZ3WMkYOSJh_vhMOS3998hbCTDDzvsHtCWWKfX8E2ijXyrvVDp0b_vlNYPmNfMv0GbrnUtguHHJPz6wfbgR-MRDt_cRgpop-4QdVn_jPtiWwoIgbOc7XcTX8tuzg1DlE6eF72QuaGPY6iFC1iasd6ltraykpIk5bsa48bitLthu-BWOCmbFvh0",
+			"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5N2IwOGI1OC00MzJlLTQxMjItYjBhNi02N2JiMmZkOTdjY2IiLCJqdGkiOiIyZDEzMjNkYTBmZjJlYTc4OWNmOGUzMDFiNWNhY2Q4M2IwMjA1ZjY3ZTg4YTg1MTA4MzNiZTFkODhlNTEzOThiYmJkNWYwZDU1ZDc1MGJlYiIsImlhdCI6MTY2Nzk0MTQzMS4xNzc4NywibmJmIjoxNjY3OTQxNDMxLjE3Nzg3OCwiZXhwIjoxNjY3OTc3NDMxLjE2NzE1MSwic3ViIjoiMSIsInNjb3BlcyI6W119.CnuziS-je2EnuLc9Pd-f4Sv25Z2q4vbf14cMT4luyOGh8lBnztAFmfl1Oj8Q25B5BadVebG6arNfoB_hFoGB3kcs8QG6DipFsZn-LrjZvYQs1qnk-sPIQ9hYRpBa90w6_gYMKn_vuk7pGn5_iYqUXX-KzGtHs_ylVEz-ZAMKvnJueR_BLbzf-xXa1Nc8rJ1ewm7gvaTFrR-WvOpkM2rurzmhnPTGXP-znFgrIvey9sPmBg3Y4VPlXazbBNVUZ0mpfiQsXxhnpw1jee2jDMIHZVEXRU5fM3c6P33FPWBuw6D0EMawvLdwNNpB-kwIaT-fKB9-M_7OYNHj9oGcExR_GFqMxXvTg8vZwLnaBlQU-FtgrwWHmFR6yS_RSjWEDqGDguvvZjs5zYxO0PbjcaBwUp-B6WnlsFMy4yLKC6YgYI6F2xdxi0UqwHFzfEZFFih5Zz7wCiTN14krnoE6_S0pRoW9cehMSru5yboIBfgQ9BZGviTVCefL3q_pe7s9xRHYU12gUdjWgNun8oXkrtL7CODQBOG4tuz0B0nYvdS-3yNZDp0C5gyvMcsI553jrPEs3yyr8dV0rAjzT8EhZ2T9hKzzTm_NCL5OLpkxgShzbEAY0p07rcH-tXjh6i-SDxUhoB5bBUXyzo_vT6vdwyGPjamvq2czD4klXNXWzf4aMYE",
+		pageLink: "http://192.168.1.3:8081/api/master/event-category/get/all",
 		data: [],
 		formData: {
 			title: "",
@@ -31,6 +32,10 @@ export const useEventCategoryStore = defineStore("eventCategoryStore", {
 		toggleAlert() {
 			this.modalAlert = false;
 		},
+		togglePage(requestURL) {
+			this.pageLink = requestURL;
+			this.getPage();
+		},
 		async onSave() {
 			this.saving = true;
 			await fetch("http://192.168.1.3:8081/api/master/event-category/create", {
@@ -46,7 +51,7 @@ export const useEventCategoryStore = defineStore("eventCategoryStore", {
 					if (!data.ok) {
 						throw Error(data.status);
 					} else {
-						this.getAll();
+						this.getPage();
 					}
 					return data.json();
 				})
@@ -60,8 +65,9 @@ export const useEventCategoryStore = defineStore("eventCategoryStore", {
 					console.log(error);
 				});
 		},
-		async getAll() {
-			await fetch("http://192.168.1.3:8081/api/master/event-category/get/all", {
+		async getPage() {
+			this.loading = true;
+			await fetch(this.pageLink, {
 				method: "GET",
 				headers: {
 					"Content-type": "application/json",
