20b2767d44a3a986ac6d5d797af117e7294b8a1a
ayaayharvey <ayaayharvey@gmail.com>
2022-11-10 13:27:31 +0800

    improved modal functionality

--- src/components/layout/Modal.vue
+++ src/components/layout/Modal.vue
@@ -3,7 +3,7 @@
 		class="fixed z-40 inset-0 bg-gray-900 bg-opacity-60 overflow-y-auto h-full w-full"
 	>
 		<div
-			class="p-5 shadow-lg rounded-md bg-white max-h-fit min-h-content absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+			class="shadow-lg rounded-md bg-white max-h-fit min-h-content absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
 			:class="`${
 				size !== 'modal-2xl'
 					? size !== 'modal-xl'
@@ -19,30 +19,35 @@
 			}`"
 		>
 			<!-- Modal Header -->
-			<div class="pb-2 flex justify-between items-center">
-				<div class="font-semibold text-lg">
+			<div
+				class="px-4 py-3 flex justify-between items-center"
+				:class="forDeletion ? 'bg-red-800' : 'bg-blue-900'"
+			>
+				<div class="font-semibold text-lg text-white">
 					{{ label }}
 				</div>
 				<i
 					@click="$emit('close-modal')"
-					class="fas fa-times cursor-pointer"
+					class="fas fa-times cursor-pointer text-white"
 				></i>
 			</div>
-			<!-- Modal Content -->
-			<div class="py-1 px-3">
-				<slot />
-			</div>
-			<!-- Modal Footer -->
-			<div
-				v-if="enableCloseButton"
-				class="w-full flex items-center mt-3"
-			>
-				<button
-					@click="$emit('close-modal')"
-					class="w-40 py-1 rounded mx-auto bg-blue-900 text-white"
+			<div class="px-5 pt-2">
+				<!-- Modal Content -->
+				<div class="px-3 pb-5">
+					<slot />
+				</div>
+				<!-- Modal Footer -->
+				<div
+					v-if="enableCloseButton"
+					class="w-full flex items-center mt-3"
 				>
-					CLOSE
-				</button>
+					<button
+						@click="$emit('close-modal')"
+						class="w-40 py-1 rounded mx-auto bg-blue-900 text-white"
+					>
+						CLOSE
+					</button>
+				</div>
 			</div>
 		</div>
 	</section>
@@ -55,6 +60,7 @@ export default {
 		label: String,
 		size: String,
 		enableCloseButton: Boolean,
+		forDeletion: Boolean,
 	},
 };
 </script>
