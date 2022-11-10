8c6fc309f7460902062d1a4536d5e2b468624919
ayaayharvey <ayaayharvey@gmail.com>
2022-11-10 13:32:58 +0800

    improved functionality + added record deletion

--- src/views/master/event-category/ViewEditForm.vue
+++ src/views/master/event-category/ViewEditForm.vue
@@ -2,37 +2,45 @@
 	<div>
 		<Modal
 			@close-modal="onClose()"
-			label="Category Details"
+			:label="
+				!formViewing
+					? 'Edit Category'
+					: !forDeletion
+					? 'View Category'
+					: 'Delete Category'
+			"
 			size="modal-xs"
+			:forDeletion="forDeletion"
 		>
-			<div
-				class="w-full justify-center flex items-center"
-				v-if="fetching"
-			>
+			<!-- <div class="w-full justify-center flex items-center">
 				<i class="fa-solid fa-spinner animate-spin text-3xl mr-3"></i>
 				<span class="font-semibold"> LOADING </span>
-			</div>
-			<div
-				class="grid grid-cols-1 gap-x-8 gap-y-3 justify-items-stretch"
-				v-if="!fetching"
-			>
+			</div> -->
+			<div class="grid grid-cols-1 gap-x-8 gap-y-3 justify-items-stretch">
+				<div
+					class="px-3 py-3 mt-3 border rounded font-semibold"
+					:class="forDeletion && 'bg-red-200 border-red-700'"
+					v-if="forDeletion"
+				>
+					Move this record to trash?
+				</div>
 				<Textbox
 					label="Category Title"
-					:value="formDataSelected.title"
-					v-model="formDataSelected.title"
+					:value="formData.title"
+					v-model="formData.title"
 					placeholder="Type text here"
 					:disabled="formViewing"
 				/>
 				<TextArea
 					label="Description"
-					:value="formDataSelected.description"
-					v-model="formDataSelected.description"
+					:value="formData.description"
+					v-model="formData.description"
 					placeholder="Type text here"
 					:disabled="formViewing"
 				/>
 				<Dropdown
 					label="Status"
-					:value="formDataSelected.status"
+					:value="formData.status"
 					@selected="dropdownSelected"
 					:selections="['active', 'inactive']"
 					:disabled="formViewing"
@@ -44,16 +52,15 @@
 					:disabled="formViewing"
 				/> -->
 			</div>
-			<div
-				class="mt-5 w-full flex items-center gap-3"
-				v-if="!fetching"
-			>
+			<div class="mt-5 w-full flex items-center gap-3">
 				<Button
 					@click="onClose()"
 					label="CLOSE"
 					color="danger"
 					icon="fa-solid fa-xmark"
-					v-if="formViewing"
+					:disabled="saving"
+					v-if="formViewing && !forDeletion"
+					class="mx-auto"
 				/>
 				<Button
 					@click="onClose()"
@@ -61,22 +68,45 @@
 					color="danger"
 					icon="fa-solid fa-xmark"
 					:disabled="saving"
-					v-if="!formViewing"
+					v-if="!formViewing && !forDeletion"
+					class="mx-auto"
 				/>
 				<Button
 					@click="toggleFormType()"
 					label="EDIT"
 					icon="fa-solid fa-pen mr-1"
-					v-if="formViewing"
+					v-if="formViewing && !forDeletion"
+					class="mx-auto"
 				/>
 				<Button
-					@click="submitForm(formDataSelected?.id)"
+					@click="submitForm(this.formData)"
 					label="UPDATE"
 					color="success"
 					icon="fa-solid fa-pen mr-1"
 					:loading="saving"
 					:disabled="saving"
-					v-if="!formViewing"
+					v-if="!formViewing && !forDeletion"
+					class="mx-auto"
+				/>
+				<Button
+					@click="onClose()"
+					label="CANCEL"
+					color="danger"
+					icon="fa-solid fa-xmark"
+					:disabled="saving"
+					v-if="forDeletion"
+					outlined
+					class="mx-auto"
+				/>
+				<Button
+					@click="onDelete(this.formData.id)"
+					label="CONFIRM"
+					color="danger"
+					icon="fa-solid fa-trash-can"
+					:loading="saving"
+					:disabled="saving"
+					v-if="forDeletion"
+					class="mx-auto"
 				/>
 			</div>
 		</Modal>
@@ -103,9 +133,20 @@ export default {
 	props: {
 		label: String,
 		formDataSelected: Object,
+		forDeletion: Boolean,
 	},
 	data() {
 		return {
+			formData: {
+				id: null,
+				title: "",
+				description: "",
+				status: "",
+			},
+			formDelete: {
+				id: null,
+				is_deleted: true,
+			},
 			roles: [],
 			formViewing: true,
 		};
@@ -115,24 +156,33 @@ export default {
 			this.formViewing = !this.formViewing;
 		},
 		dropdownSelected(value) {
-			this.formDataSelected.status = value;
+			this.formData.status = value;
+		},
+		onDelete(id) {
+			this.formDelete.id = id;
+			this.submitForm(this.formDelete);
 		},
 		onClose() {
 			this.$emit("close-form");
 		},
 	},
+	created() {
+		this.formData.id = this.formDataSelected.id;
+		this.formData.title = this.formDataSelected.title;
+		this.formData.description = this.formDataSelected.description;
+		this.formData.status = this.formDataSelected.status;
+	},
 	setup() {
 		const eventCategoryStore = useEventCategoryStore();
 		// state and getters
-		const { formDataSelected, saving, fetching } =
-			storeToRefs(eventCategoryStore);
+		const { data, formDataSelected, saving } = storeToRefs(eventCategoryStore);
 		// actions
 		const { submitForm } = eventCategoryStore;
 		// return
 		return {
+			data,
 			formDataSelected,
 			saving,
-			fetching,
 			submitForm,
 		};
 	},
